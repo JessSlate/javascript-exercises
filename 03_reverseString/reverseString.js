3a42fcf203d6ce3e4b14d8e33a93a3144d018003
Jess Slate <jessmslate@gmail.com>
2023-01-02 20:24:35 -0500

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let result = input.split("");
+    let output = "";
+    for(let i = (result.length -1); i >= 0; i--){
+        output += result[i];
+    }
+    return output;
+    
 };
 
 // Do not edit below this line
