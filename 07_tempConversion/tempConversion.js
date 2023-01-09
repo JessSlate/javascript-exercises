44270fe3d1b3fdea074f82b948d7c596ac99e704
Jess Slate <jessmslate@gmail.com>
2023-01-09 00:28:18 -0500

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let conversionConstant = 5/9;
+  let celsiusTemp = (tempFahrenheit - 32) * conversionConstant;
+  return Math.round(celsiusTemp * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let conversionConstant = 9/5;
+  let fahrenheitTemp = (tempCelsius * conversionConstant) + 32;
+  return Math.round(fahrenheitTemp * 10) / 10;
 };
 
 // Do not edit below this line
