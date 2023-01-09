2ba8aeb6688425e26d0bd48cf885ff423aa4b513
Jess Slate <jessmslate@gmail.com>
2023-01-09 01:13:05 -0500

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (testString) {
+    testString = testString.toLowerCase();
+    testString = testString.replace(/[^a-z]/gi, ''); //regex removes non alphabet chars and spaces
 
+    let originalString = testString;
+    let reversedString = "";
+    
+    testString = testString.split(''); //split into char array
+
+    for(let i = (testString.length - 1); i >= 0; i--){
+        reversedString += testString[i];
+    }
+
+    if(originalString == reversedString){
+        return true;
+    } else return false;
 };
 
 // Do not edit below this line
