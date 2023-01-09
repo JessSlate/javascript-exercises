72e93831b2eea8956bd1612bfc32aa0e8794e16a
Jess Slate <jessmslate@gmail.com>
2023-01-09 00:04:34 -0500

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if(!(Number.isInteger(start) && Number.isInteger(end))){ //return error with non-number args
+        return "ERROR";
+    }
+
+    if(end < 0 || start < 0){
+        return "ERROR";
+    }
+
+    if(end < start){ //works with higher number first
+        let temp = end;
+        end = start;
+        start = temp;
+    }
+
+    for(let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
+    
 };
 
 // Do not edit below this line
