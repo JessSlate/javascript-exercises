fc3c84370ab86316e1aacde2a4b85a4bd7eac482
Jess Slate <jessmslate@gmail.com>
2023-01-09 00:15:33 -0500

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(year % 4 == 0){ //leap years are divisible by four
+        if(year % 100 == 0 && !(year % 400 == 0)){ //leap years are not divisible by 100, unless also divisible by 400
+            return false;
+        }
+        else return true;
+    } else return false;
 
 };
 
