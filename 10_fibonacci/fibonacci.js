527610c22d38cecbc507bde7aec74a24e51baf9f
Jess Slate <jessmslate@gmail.com>
2023-01-10 22:52:12 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(placement) {
+    if(placement < 0){
+        return "OOPS";
+    }
+
+    let currentNumber = 0,
+        lastNumber = 1;
+    
+        for(let i = 0; i < placement; i++){
+        let placeholder = currentNumber;
+        currentNumber += lastNumber;
+        lastNumber = placeholder;
+    }
+    return currentNumber;
 
 };
 
