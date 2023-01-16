1ca99507a09661cea496a512c22f26f40114fe4b
Jess Slate <jessmslate@gmail.com>
2023-01-16 13:09:31 -0500

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    /*
+    let titleList = [];
+    for(book of books){
+        titleList.push(book.title);
+    }
+    */
+    const titleList = books.map(book => book.title);
+    return titleList;
 };
 
 // Do not edit below this line
