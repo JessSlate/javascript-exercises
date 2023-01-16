937a8429aa7410ac1f87b97d4142fc186beb6ae1
Jess Slate <jessmslate@gmail.com>
2023-01-16 13:30:22 -0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((person1, person2) => {
+        let lifespan1, lifespan2;
+        if(!person1.yearOfDeath){
+            lifespan1 = new Date().getFullYear() - person1.yearOfBirth;
+        } else lifespan1 = person1.yearOfDeath - person1.yearOfBirth;
+        if(!person2.yearOfDeath){
+            lifespan2 = new Date().getFullYear() - person2.yearOfBirth;
+        } else lifespan2 = person2.yearOfDeath - person2.yearOfBirth;
+        return lifespan1 > lifespan2 ? -1:1;
+    });
+    return(people[0]);
 };
 
 // Do not edit below this line
